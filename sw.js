b6f0da3d61b09d928aca8e641186aa899b34996f
noot101 <noot101@hotmail.com>
2026-09-26 18:56:37 +0200

    Add files via upload
    PWA app

---
+++ sw.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = "loopwise-v1.7.0";
+const PRECACHE = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(PRECACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  const url = new URL(event.request.url);
+  // Alleen eigen bestanden cachen; routeberekening en kaarttegels altijd live ophalen.
+  if(url.origin !== self.location.origin || event.request.method !== "GET"){
+    return;
+  }
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      const network = fetch(event.request).then(resp => {
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, resp.clone()));
+        return resp;
+      }).catch(() => cached);
+      return cached || network;
+    })
+  );
+});
