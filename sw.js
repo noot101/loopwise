966a7f9158fc7768865ce8069d70e42db95ffac3
noot101 <noot101@hotmail.com>
2026-09-26 19:02:22 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "loopwise-v1.7.0";
+const CACHE_NAME = "loopwise-v1.8.0";
 const PRECACHE = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", event => {
